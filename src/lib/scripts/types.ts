6a4e72d60c94d67aa5e5d0053c40a48047bc847a
Martin Matovu <matovius@proton.me>
2025-04-27 01:24:57 +0300

    feat: add JSDoc comment for `RandomConfigs` type

--- src/lib/scripts/types.ts
+++ src/lib/scripts/types.ts
@@ -1,4 +1,9 @@
-export type Configs = {
+/**
+ * The type for the random character generator configs.
+ *
+ * Includes a `passwordLength` number, and booleans for `includesNumbers` and `includesSymbols`
+ */
+export type RandomConfigs = {
 	passwordLength: number;
 	includesNumbers: boolean;
 	includesSymbols: boolean;
